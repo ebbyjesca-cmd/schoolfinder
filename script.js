f4a45cfb8d7466661a503be70edd59264f2dee11
Ebby <ebbyjesca@gmail.com>
2026-04-20 21:04:24 -0700

    signup javascript

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ if  (signupbtn) {
         alert("Account created! You can now login")
     });
 }
+
+
+const loginbtn = document.getElementById("loginbtn");
+console.log("login button:", loginbtn);
+
+if (loginbtn) {
+    loginbtn.addEventListener("click", () => {
+        console.log("button clicked");
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        const savedUser = JSON.parse(localStorage.getItem("user"));
+
+        if (!savedUser) {
+            alert("No account found. please sign up.");
+            return;
+        }
+
+        if (email ===savedUser.email && password === savedUser.password) {
+            localStorage.setItem("LoggedIn", "true");
+
+            alert("login successful!");
+            window.location.href = "index.html";
+        } else {
+            alert("invalid credentials");
+        }
+    });
+}
